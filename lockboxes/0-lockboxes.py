dc786345d97109889a41f97b2dc4acec9f53fdc8
annlaure02 <alaure.g02@gmail.com>
2023-08-11 21:02:06 +0200

    condition to fix list index out of range

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,9 +8,10 @@ def canUnlockAll(boxes):
     keys_list = [0]
 
     for element in keys_list:
-        for key in boxes[element]:
-            if key not in keys_list:
-                keys_list.append(key)
+        if element < n:
+            for key in boxes[element]:
+                if key not in keys_list:
+                    keys_list.append(key)
     if len(keys_list) == n:
         return True
     return False
