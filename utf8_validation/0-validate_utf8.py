0ac02bc689db739c8f2bbaaaea6226553256cbcf
annlaure02 <alaure.g02@gmail.com>
2023-09-13 14:08:23 +0200

    validate_utf8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@ Determines if a given data set represents a valid UTF-8 encoding
 
 def validUTF8(data):
     for i in data:
-      if i >= 0 and i <= 127:
-        return True
-      else:
-        return False
+        if i >= 0 and i <= 127:
+            return True
+        else:
+            return False
